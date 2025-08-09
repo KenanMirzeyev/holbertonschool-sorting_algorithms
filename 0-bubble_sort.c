d6238489d12cb8afd369acf04d2693ae6366ba87
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-08-09 08:40:31 +0000

    .

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - bubble
+ * @array: a
+ * @size: s
+ *
+ * Return: array
+ */
+void bubble_sort(int *array, size_t size)
+{
+        size_t i, j;
+        int temp;
+        int swap;
+
+        if (!array || size < 2)
+                return;
+
+        for (i = 0 ; i < size ; i++)
+        {
+                swap = 0;
+                for (j = 0 ; j < size - i - 1 ; j++)
+                {
+                        if (array[j] > array[j + 1])
+                        {
+                                temp = array[j];
+                                array[j] = array[j + 1];
+                                array[j + 1] = temp;
+                                print_array(array, size);
+                                swap = 1;
+                        }
+                }
+                if (!swap)
+                        break;
+        }
+}
