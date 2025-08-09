7222738648cd40207419f6954dd94f581e58f28d
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-08-09 08:43:30 +0000

    .

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,28 +11,28 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-        size_t i, j;
-        int temp;
-        int swap;
+	size_t i, j;
+	int temp;
+	int swap;
 
-        if (!array || size < 2)
-                return;
+	if (!array || size < 2)
+		return;
 
-        for (i = 0 ; i < size ; i++)
-        {
-                swap = 0;
-                for (j = 0 ; j < size - i - 1 ; j++)
-                {
-                        if (array[j] > array[j + 1])
-                        {
-                                temp = array[j];
-                                array[j] = array[j + 1];
-                                array[j + 1] = temp;
-                                print_array(array, size);
-                                swap = 1;
-                        }
-                }
-                if (!swap)
-                        break;
-        }
+	for (i = 0 ; i < size ; i++)
+	{
+		swap = 0;
+		for (j = 0 ; j < size - i - 1 ; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+				swap = 1;
+			}
+		}
+		if (!swap)
+			break;
+	}
 }
